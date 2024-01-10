b519b78f70012af933237929ffbf44f0fdb9582c
navigaid <navigaid@gmail.com>
2024-01-10 05:24:16 +0000

    make snapshot

--- snapshot.sh
+++ snapshot.sh
@@ -9,7 +9,7 @@ popd
 cp -r net/internal/quic/* .
 
 git grep golang.org/x/net\
-/internal/quic | cut -d : -f 1 | xargs -L1 sed -i -e s,golang.org/x/net\
+/internal/quic | cut -d : -f 1 | grep -v README.md | xargs -L1 sed -i -e s,golang.org/x/net\
 /internal/quic,github.com/webtransport/quic,g
 
 go mod tidy
